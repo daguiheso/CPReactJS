f2fbbe496683eca5eafe2fe861fbacd4d27df2bb
Daniel H <frontend@reddesignsystems.com>
2016-05-17 11:35:35 -0500

    Config socket.io and serve

--- index.js
+++ index.js
@@ -18,5 +18,15 @@ app.get('/', (req,res) => {
 })
 
 let server = http.createServer(app).listen(port, () => {
-	console.log('El servidor esta escuchando en el puerto')
+	console.log(`El servidor está escuchando en el puerto ${port}`)
+})
+
+
+const io = engine.listen(server);
+
+/*escuhar nuevas conexiones*/
+io.on('connection', (socket) => {  /*vamos a recibir un socket como parametro*/
+	socket.on('message', (msg) => { /*nos suscribimos al metodo message y en el vamos a recibir un msg que va a venir del lado del cliente*/
+		io.emit('message', msg)  /*lo que hacemos es emitir ese mensage a todos los clientes que esten suscritos como un evento de tipo message*/
+	})  
 })
